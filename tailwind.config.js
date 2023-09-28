32553db73d6b48a312750cdac4458cde8a8fe6c2
Diana Petrovčíková <63262388+petrovcikovadiana@users.noreply.github.com>
2023-09-28 14:12:28 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "3.75rem",
+      "7xl": "4.5rem",
+      "8xl": "6rem",
+      "9xl": "8rem",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", "sans-serif"],
+      },
+
+      colors: {
+        background: "#191919",
+        goldenYellow: "#f8cc8c",
+        lightBlack: "#212529",
+        darkWhite: "#ddd",
+      },
+    },
+  },
+  plugins: [],
+};
